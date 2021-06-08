cd0f1946f607161df2bd21d682319378d03864c6
shash-2106 <raoshashwati@gmail.com>
2021-06-08 19:48:12 +0530

    p82

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import SignupLogin from './screens/SignupLoginScreen';
+import DonorScreen from './screens/DonorScreen';
+import VolunteerScreen from './screens/VolunteerScreen';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import SettingScreen from './screens/SettingScreen'
+import CustomSideBarMenu from './Component/CustomSideBarMenu';
+import {TabNavigator} from './Component/AppTabNavigator';
+import {AppDrawerNavigator} from './Component/AppDrawerNavigator'
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+     
+      <AppContainer/>
+
   );
 }
+}
 
+
+const SwitchNavigator = createSwitchNavigator({SignupLogin:{screen:SignupLogin},DrawerTab:{screen:AppDrawerNavigator}})
+const AppContainer = createAppContainer(SwitchNavigator)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
